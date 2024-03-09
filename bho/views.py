b48e972adcc47799111dd32a5eaabf5ba0b28942
Blaise hala <blaisehala@gmail.com>
2024-03-10 02:45:48 +0300

    modified files

--- bho/views.py
+++ bho/views.py
@@ -7,24 +7,26 @@ posts=[
         'author': 'Blaise Haala',
         'title': 'Blog post 1',
         'content': 'Twende',
-        'date_time':'August 27 2024'
+        'date_posted':'August 27 2024'
     },
 
       {
-        'author': 'Blaise Haala',
-        'title': 'Blog post 1',
-        'content': 'Twende',
-        'date_time':'August 27 2024'
+        'author': 'Manu Obare',
+        'title': 'Blog post 2',
+        'content': 'Twendete',
+        'date_posted':'September 30 2024'
     },
 ]
 
 
 # Create your views here.
 def home(request):
-    return render (request, 'bho/home.html')
+    context = {
+        'posts':posts 
+    }
+    return render (request, 'bho/home.html', context)
     
 
 
 def about(request):
-    return render (request, 'bho/about.html')
-    
+    return render (request, 'bho/about.html', {'title':'About'})    

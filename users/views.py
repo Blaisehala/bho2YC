63cddc80042e405a8f487b31e794b3b8dc6fe364
Blaise hala <blaisehala@gmail.com>
2024-03-10 23:19:49 +0300

    BUGS

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages 
+from .forms import UserRegisterForm
 
 # Create your views here.
 
@@ -9,17 +10,17 @@ from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
             form.save()
+            username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect ('bho-home')
 
     else:
-        form = UserCreationForm()
-    return render(request, 'bho/about.html', {'form': form})
-    context_instance=RequestContext(request)
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+    
    
 
  

d2780b73d386de40d616eb78c9b144d8e32b4353
Blaise hala <blaisehala@gmail.com>
2024-03-10 06:05:44 +0300

    build:updates

--- bho/views.py
+++ bho/views.py
@@ -3,22 +3,6 @@ from .models import Post
 
 
 
-posts=[
-    {
-        'author': 'Blaise Haala',
-        'title': 'Blog post 1',
-        'content': 'Twende',
-        'date_posted':'August 27 2024'
-    },
-
-      {
-        'author': 'Manu Obare',
-        'title': 'Blog post 2',
-        'content': 'Twendete',
-        'date_posted':'September 30 2024'
-    },
-]
-
 
 # Create your views here.
 def home(request):

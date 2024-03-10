cd89c22919ab80d8bf582be6881735aec9d2c856
Blaise hala <blaisehala@gmail.com>
2024-03-10 06:14:48 +0300

    build:creates UserApp

--- Dash_project/settings.py
+++ Dash_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'bho.apps.BhoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
